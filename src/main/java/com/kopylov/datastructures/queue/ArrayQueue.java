4095a8240f650127a99242333f62c03cbeedcf70
Vadym <laurencekiev@gmail.com>
2022-07-01 21:41:00 +0300

    Change pom,and add ArrayQueue

--- src/main/java/com/kopylov/datastructures/queue/ArrayQueue.java
+++ src/main/java/com/kopylov/datastructures/queue/ArrayQueue.java
@@ -1,38 +1,80 @@
 package com.kopylov.datastructures.queue;
 
 public class ArrayQueue implements Queue{
+    private Object[] array;
+    private int size;
+    private int count;
+
+    public ArrayQueue() {
+        array = new Object[10];
+    }
+
     @Override
     public void enqueue(Object value) {
+        array[size] = value;
+        size++;
 
     }
 
     @Override
     public Object dequeue() {
-        return null;
+        if(isEmpty()){
+            throw new IllegalStateException("Stack is empty");
+        }
+        Object result = array[0];
+        for(int i = 0;i < size;i++){
+            array[i] = array[i+1];
+        }
+        size--;
+        return result;
     }
 
     @Override
     public Object peak() {
-        return null;
+        return array[count];
     }
 
     @Override
     public boolean contains(Object value) {
+        for(int i = 0;i < size;i++){
+            Object arrayInQueue = array[i];
+            if(arrayInQueue.equals(value)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isEmpty() {
+        if(size == 0){
+            return true;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+
+        return size;
     }
 
     @Override
     public void clear() {
-
+        for(int i = 0;i < size;i++){
+            array[i] = null;
+        }
+        size = 0;
+    }
+    @Override
+    public String toString() {
+        String result = "";
+            for(int i = 0;i < size;i++){
+                result = result + array[i];
+                if(i < size-1){
+                    result = result + ",";
+                }
+            }
+            return "[" + result + "]";
     }
 }
---
+++ src/test/java/com/kopylov/datastructures/queue/ArrayQueueTest.java
@@ -0,0 +1,89 @@
+package com.kopylov.datastructures.queue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayQueueTest {
+    ArrayQueue arrayQueue = new ArrayQueue();
+    @Test
+    public void testQueueAndDequeueInArrayAndChekSize(){
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+        assertEquals(3,arrayQueue.size());
+        assertEquals("1",arrayQueue.dequeue());
+        assertEquals("2",arrayQueue.dequeue());
+        assertEquals("3",arrayQueue.dequeue());
+        assertEquals(0,arrayQueue.size());
+    }
+
+    @Test
+    public void testPeekWorkingCorrectly(){
+
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+
+        assertEquals(3,arrayQueue.size());
+        assertEquals("1",arrayQueue.peak());
+        assertEquals(3,arrayQueue.size());
+    }
+    @Test
+    public void testThrowExceptionIfArrayDequeueOnEmptyState(){
+        Assertions.assertThrows(IllegalStateException.class,() ->{
+            arrayQueue.dequeue();
+        });
+    }
+
+    @Test
+    public void testReturnTrueIfArrayIsEmpty(){
+
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testReturnTrueIfArrayContainsValue(){
+
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+
+        assertTrue(arrayQueue.contains("2"));
+    }
+    @Test
+    public void testReturnFalseIfArrayNotContainsValue(){
+
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+
+        assertFalse(arrayQueue.contains("4"));
+    }
+    @Test
+    public void testReturnFalseIfArrayIsEmpty(){
+
+        assertFalse(arrayQueue.contains("1"));
+    }
+    @Test
+    public void testClearArrayAfterUse(){
+
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+
+        arrayQueue.clear();
+        assertTrue(arrayQueue.isEmpty());
+    }
+    @Test
+    public void testQueueWithValueToString(){
+        arrayQueue.enqueue("1");
+        arrayQueue.enqueue("2");
+        arrayQueue.enqueue("3");
+
+        assertEquals(3,arrayQueue.size());
+        assertEquals("[1,2,3]",arrayQueue.toString());
+    }
+}
